02ebcb5c4e0094a071f3f92adea1f21b99424343
FedericaCampo97 <federica.campo97@gmail.com>
2023-10-15 23:04:56 +0200

    Exercise complete

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
 </head>
 
@@ -12,8 +13,23 @@
     <div id="app">
         <div class="container">
             <div class="row">
-                <div class="col" v-for="disk in disks">
-                    <img :src="disk.poster" alt="">
+                <div class="col-4 p-5" v-for="disk in disks">
+                    <div class="card text-center p-3">
+                        <img :src="disk.poster" class="mx-auto " alt="">
+                        <div class="card_text">
+                            <h6 class="mt-3">
+                                {{disk.title}}
+                            </h6>
+                            <p class="mb-1">
+                                {{disk.author}}
+                            </p>
+                            <p>
+                                {{disk.year}}
+                            </p>
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
         </div>
